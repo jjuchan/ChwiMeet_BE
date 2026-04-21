6e0df2275590c06c419461e3890dd1ef5d7712ce
jjuchan <yjc7241@naver.com>
2026-04-21 15:36:32 +0900

    chore: 개인 배포를 위한 워크플로우 수정

--- backend/src/main/java/com/back/global/app/AppConfig.java
+++ backend/src/main/java/com/back/global/app/AppConfig.java
@@ -1,29 +1,32 @@
 package com.back.global.app;
 
-import com.back.standard.util.json.JsonUt;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.back.standard.util.json.JsonUt;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.annotation.PostConstruct;
+
 @Configuration
 public class AppConfig {
-    private static ObjectMapper objectMapper;
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	private static ObjectMapper objectMapper;
 
-    @Autowired
-    public void setObjectMapper(ObjectMapper objectMapper) {
-        AppConfig.objectMapper = objectMapper;
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @PostConstruct // 빈(Bean)이 생성되고 의존성 주입이 끝난 직후에 딱 한 번 호출되는 초기화 훅(hook)
-    public void postConstruct() {
-        JsonUt.objectMapper = objectMapper;
-    }
+	@Autowired
+	public void setObjectMapper(ObjectMapper objectMapper) {
+		AppConfig.objectMapper = objectMapper;
+	}
+
+	@PostConstruct // 빈(Bean)이 생성되고 의존성 주입이 끝난 직후에 딱 한 번 호출되는 초기화 훅(hook)
+	public void postConstruct() {
+		JsonUt.objectMapper = objectMapper;
+	}
 }
