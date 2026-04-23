e6e5393d98c3c86d979f7f43a21bb5bb34193b3e
jjuchan <yjc7241@naver.com>
2026-04-23 19:29:53 +0900

    refactor: 개인 배포 세팅

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,24 +1,24 @@
 variable "region" {
-  description = "region"
+  description = "AWS 리전"
   default     = "ap-northeast-2"
 }
 
 variable "prefix" {
-  description = "Prefix for all resources"
-  default     = "team1" // 팀 이름으로 사용
+  description = "리소스 이름 앞에 붙을 접두어"
+  default     = "chwimeet-jjuchan"
 }
 
-variable "team_tag_value" { // 팀 태그 값 추가
-  description = "Value for the 'Team' tag (e.g., devcos-team01)"
-  default     = "devcos-team01"
+variable "team_tag_value" {
+  description = "리소스 태그 (Team)"
+  default     = "personal"
 }
 
 variable "app_1_domain" {
-  description = "app_1 domain"
-  default     = "api.chwimeet.store"
+  description = "백엔드 API 도메인"
+  default     = "api.chwimeet.jjuchan.shop" 
 }
 
 variable "key_name" {
-  description = "EC2 Key Pair name"
-  default     = "team1_key" // 사용할 키페어 이름
+  description = "AWS EC2 키페어 이름"
+  default     = "chwimeet-key" 
 }
